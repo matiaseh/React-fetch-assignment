332718738bfa40d6f34371ea02c0d2141abfe273
Matias Hynnä <38789985+matiaseh@users.noreply.github.com>
2021-02-16 16:46:23 +0200

    Final build

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,42 +1,37 @@
-import { DataGrid } from '@material-ui/data-grid';
-import React from 'react';
+import { DataGrid } from "@material-ui/data-grid";
+import React from "react";
 
 const columns = [
-
-    {
-        field: 'conversation_count',
-        headerName: 'conversation_count',
-        width: 200,
-    },
-    {
-        field: 'missed_chat_count',
-        headerName: 'missed_chat_count',
-        width: 200
-    },
-    {
-        field: 'visitors_with_conversation_count',
-        headerName: 'visitors_with_conversation_count',
-        width: 300
-    },
-    {
-        field: 'date',
-        headerName: 'Date',
-        width: 200
-    },
-
-
+  {
+    field: "conversation_count",
+    headerName: "conversation_count",
+    width: 200,
+  },
+  {
+    field: "missed_chat_count",
+    headerName: "missed_chat_count",
+    width: 200,
+  },
+  {
+    field: "visitors_with_conversation_count",
+    headerName: "visitors_with_conversation_count",
+    width: 300,
+  },
+  {
+    field: "date",
+    headerName: "Date",
+    width: 200,
+  },
 ];
 
-
-
 const DataTable = (props) => {
-    const rows = props.rows;
+  const rows = props.rows;
 
-    return (
-        <div style={{ height: 335, margin: "15px" }}>
-      <DataGrid rows={rows} columns={columns} pageSize={4}/>
+  return (
+    <div style={{ height: 335, margin: "15px" }}>
+      <DataGrid rows={rows} columns={columns} pageSize={4} />
     </div>
-    )
-}
+  );
+};
 
 export default DataTable;
--- src/components/ErrorSnackbar.js
+++ src/components/ErrorSnackbar.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Snackbar from '@material-ui/core/Snackbar';
-import MuiAlert from '@material-ui/lab/Alert';
+import React from "react";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
 const ErrorSnackBar = (props) => {
-    const open = props.open;
-    const onClose = props.onClose;
-  
+  const open = props.open;
+  const onClose = props.onClose;
+
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
@@ -19,12 +19,12 @@ const ErrorSnackBar = (props) => {
   };
 
   return (
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="error">
-          Error fetching data!
-        </Alert>
-      </Snackbar>
+    <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity="error">
+        Error fetching data!
+      </Alert>
+    </Snackbar>
   );
-}
+};
 
 export default ErrorSnackBar;
